53e3eae5a300fca12e40963a42466e8ea0abb212
Rashika Patel <rashika_patel@hotmail.com>
2018-11-05 16:14:20 +0000

    feat: reset start time for sounds

--- Drum-kit/main.js
+++ Drum-kit/main.js
@@ -1,18 +1,20 @@
 
 function removeTransition(e) {
   if (e.propertyName === "transform") {
-    this.classList.remove("playing")
+    this.classList.remove("playing");
   }
 }
 
 function playSound(e) {
-  const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`)
-  key.classList.add("playing")
-  audio.play()
-  const keys = document.querySelectorAll('.key')
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+  const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
 
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 }
 
-window.addEventListener('keydown', playSound)
+window.addEventListener('keydown', playSound);
